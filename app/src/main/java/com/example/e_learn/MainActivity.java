a7ac63fecfb15e3a43a0bc16c92413315ab804ce
Bidooooooooooooo <ana_bedo>
2022-04-09 19:22:13 +0200

    disign completing

--- app/src/main/java/com/example/e_learn/MainActivity.java
+++ app/src/main/java/com/example/e_learn/MainActivity.java
@@ -9,7 +9,8 @@ import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-
+    VideoView mVideoView;
+    MediaController mediaController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +21,13 @@ public class MainActivity extends AppCompatActivity {
         final VideoView videoView=(VideoView) findViewById(R.id.video_view2);
         //https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4
 
-/*        String path="D:/ODC/E-learn/app/src/main/res/raw/videosview.MOV";
+    /*  String path="D:/ODC/E-learn/app/src/main/res/raw/videosview.MOV";
         Uri u = Uri.parse(path);*/
 
         mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
-/*
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+       videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mediaPlayer) {
                 mediaPlayer.start();
@@ -37,11 +38,26 @@ public class MainActivity extends AppCompatActivity {
             public void onCompletion(MediaPlayer mediaPlayer) {
 
             }
-        });*/
-        videoView.setVideoPath("https://web.law.duke.edu/cspd/contest/videos/Framed-Contest_Documentaries-and-You.mp4");
-        videoView.start();
+        });
+       /* String u="https://www.pexels.com/video/a-woman-looking-through-the-microscope-5752729/";
+        mVideoView.setVideoURI(Uri.parse(u));
+       // videoView.setVideoPath("https://www.pexels.com/video/a-woman-looking-through-the-microscope-5752729/");
+        videoView.start();*/
+
+       mediaController = new MediaController(this);
+        mVideoView = findViewById(R.id.video_view2);
+        mediaController.setAnchorView(mVideoView);
+        Uri localUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.v3);
+
+        mVideoView.setVideoURI(localUri);
+        mVideoView.setMediaController(mediaController);
+
+        mVideoView.start();
+
 
 
     }
+
+
 }
 
